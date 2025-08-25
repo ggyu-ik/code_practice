0c859571477b2540b9b3c7ab9770f8bf239362f5
ggyu-ik <ggye0723@gmail.com>
2025-08-25 10:18:53 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -6,8 +6,32 @@ using namespace std;
 int solution(string s) 
 {
     int answer = 0;
+    int num = 0;
     
-    answer = stoi(s);
+    if(s[0] == '-') // 첫 문자가 - 일때
+    {
+        for(int i = 1 ; i < s.size() ; ++i) // 두번째 문자부터 시작
+        {
+            num = num * 10 + (s[i] - '0'); // 문자형 숫자에 '0' = 48을 빼면 정수형 숫자가 됨.
+        }
+        answer = num * -1;
+    }
+    else if(s[0] == '+')
+    {
+        for(int i = 1 ; i <s.size() ; ++i)
+        {
+            num = num * 10 + (s[i] - '0');
+        }
+        answer = num;
+    }
+    else
+    {
+        for(int i = 0 ; i < s.size() ; ++i)
+        {
+            num = num * 10 + (s[i] - '0');
+        }
+        answer = num;
+    }
     
     return answer;
 }
